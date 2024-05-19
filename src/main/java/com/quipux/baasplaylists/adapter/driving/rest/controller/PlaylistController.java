a52c42f3a1c6e5913cc4d0031372794cd2103420
Sebastian Valencia C <54197188+yipson@users.noreply.github.com>
2024-05-19 01:06:31 -0500

    Revert "Adding spotify getGenres consume service also with getToken spotify s…"

--- src/main/java/com/quipux/baasplaylists/adapter/driving/rest/controller/PlaylistController.java
+++ src/main/java/com/quipux/baasplaylists/adapter/driving/rest/controller/PlaylistController.java
@@ -3,13 +3,10 @@ package com.quipux.baasplaylists.adapter.driving.rest.controller;
 import com.quipux.baasplaylists.adapter.driving.rest.model.CreatePlaylistDto;
 
 import com.quipux.baasplaylists.adapter.driving.rest.model.DescriptionDto;
-import com.quipux.baasplaylists.adapter.driving.rest.model.GenresDto;
 import com.quipux.baasplaylists.adapter.mapper.PlaylistMapper;
-import com.quipux.baasplaylists.domain.model.Genres;
 import com.quipux.baasplaylists.domain.model.Playlist;
 import com.quipux.baasplaylists.domain.usecase.port.PlaylistPort;
-import com.quipux.baasplaylists.utils.constant.Constant;
-import com.quipux.baasplaylists.utils.exception.BadRequestException;
+import com.quipux.baasplaylists.utils.BadRequestException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
@@ -27,7 +24,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController()
-@RequestMapping(Constant.PLAYLIST_BASE_PATH)
+@RequestMapping("/api/v1/lists")
 public class PlaylistController {
 
     private final PlaylistPort playlistPort;
@@ -36,7 +33,7 @@ public class PlaylistController {
         this.playlistPort = playlistPort;
     }
 
-    @PostMapping(value = Constant.LISTS_PATH, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CreatePlaylistDto> createPlaylist(@RequestBody CreatePlaylistDto createPlaylistDto){
         Playlist playlist = this.createPlaylistBuildRequest(createPlaylistDto);
         return this.createPlaylistBuildResponse(playlist);
@@ -63,7 +60,7 @@ public class PlaylistController {
                 .body(createdPlaylist);
     }
 
-    @GetMapping(value = Constant.LISTS_PATH, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<CreatePlaylistDto>> getPlayLists(){
         List<Playlist> playlists =  playlistPort.getPlayLists();
         return ResponseEntity.ok(playlists.stream()
@@ -71,29 +68,17 @@ public class PlaylistController {
                 .collect(Collectors.toList()));
     }
 
-    @GetMapping(value = Constant.LISTS_NAME_PATH, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{list-name}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<DescriptionDto> getDescription(@PathVariable("list-name") String listName){
         return ResponseEntity.ok(DescriptionDto.builder()
                         .description(playlistPort.getDescription(listName))
                 .build());
     }
 
-    @DeleteMapping(value = Constant.LISTS_NAME_PATH, produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{list-name}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> deletePlayList(@PathVariable("list-name") String listName){
         playlistPort.delete(listName);
         return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping(value = Constant.GET_SPOTIFY_GENRES, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<GenresDto> getGenres(){
-        return ResponseEntity.ok(this.getGenresBuildResponse());
-    }
-
-    private GenresDto getGenresBuildResponse() {
-        Genres genres = playlistPort.getGenders();
-        return GenresDto.builder()
-                .genres(genres.getGenres())
-                .build();
     }
 
 }
--- src/main/java/com/quipux/baasplaylists/utils/PlayListNotFoundException.java
+++ src/main/java/com/quipux/baasplaylists/utils/PlayListNotFoundException.java
@@ -1,4 +1,4 @@
-package com.quipux.baasplaylists.utils.exception;
+package com.quipux.baasplaylists.utils;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
